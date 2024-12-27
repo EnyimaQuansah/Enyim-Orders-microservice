1285cce1bb6b96d20ecdb0fe8522fd94746eaaf0
Enyima Quansah <quansahenyima@gmail.com>
2024-12-27 17:56:28 +0000

    vercel json addition

---
+++ app.js
@@ -0,0 +1,47 @@
+// orders microservice server.js
+require('dotenv').config();
+const express = require("express");
+const { connectRabbitMQ } = require("./config/rabbitmq");
+const orderRouter = require("./routes/orderRouter");
+const productRouter = require("./routes/productRouter");
+const { connectOrderDB } = require("./config/db");
+const { consumeProductEvents } = require("./events/consumeProductEvents");
+const syncProductCache = require("./services/productCacheSyncService");
+const { initializeAndSyncProducts } = require('./services/elasticsearchSync');
+const imageRouter = require('./routes/imageRoutes');
+
+
+const app = express();
+app.use(express.json());
+
+
+
+(async () => {
+  try {
+
+    // Connect to the main Order DB
+    connectOrderDB();
+	
+	// Start ProductCache Sync Service
+    await syncProductCache();
+
+    await connectRabbitMQ();
+    // await consumeProductEvents();
+	
+
+	initializeAndSyncProducts().catch((error) => {
+	  console.error('Failed to initialize and sync:', error);
+	});
+
+  } catch (err) {
+    console.error("Failed to start server:", err.message);
+    process.exit(1);
+  }
+})();
+
+// Routes
+app.use("/orders", orderRouter);
+app.use("/products", productRouter);
+app.use('/images', imageRouter);
+
+module.exports = app;
